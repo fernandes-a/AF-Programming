723ace5e10430b799600c7cce2a80b364571ff45
Abby Fernandes <fernande.a@maristcollege.school.nz>
2020-06-30 09:09:04 +1200

    First function set - on button click the sections will change

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-// alert("Hello World!");
+function nextSection(currentSection, nextSection) { // This function will hide the section and show the next when clicked
+    document.querySelector('#' + currentSection).style.display = "none";
+    document.querySelector('#' + nextSection).style.display = "block";
+}
