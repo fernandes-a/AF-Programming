7a9eca66a208342290dbae6e089b3beed37d18b9
Abby Fernandes <fernande.a@maristcollege.school.nz>
2020-07-17 10:18:21 +1200

    allowContinue function added - if the users score is > 3 then they can move on, this function will work for all sections, still havent made the markSection function able to do this

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // These variables provide the answers that the test will be marked against
 const SECTION_ONE_ANSWERS = ["b", "d", "c", "c", "b"]; 
 
-let score = 0;
+let globalScore = 0;
 
 // This function will hide the current section and show the next when clicked
 function nextSection(currentSection, nextSection) { 
@@ -10,17 +10,36 @@ function nextSection(currentSection, nextSection) {
 }
 
 // This function will check that the answers inputed by the user are correct
-function markSection() { 
+function markSection(sectionNumber) { 
     let i = 1;
+    let sectionScore = 0;
     while (i < 6) {
         let input = document.querySelector("#answer" + i);
         input = input.value;
 
         if (input == SECTION_ONE_ANSWERS[(i-1)]) {
-            alert(`question ${i} correct!`)
+            document.querySelector("#answer" + i).style = "background-color:green;";
+            sectionScore++;
         } else {
-            alert(`question ${i} incorrect`)
+            document.querySelector("#answer" + i).style = "background-color:red;";
         }
         i++;
+    }
+
+    allowContinue(sectionNumber, sectionScore);
+
+    globalScore=+ sectionScore;
+    return i;
+}
+
+// This function will reveal the continue button if they get more than 3 correct answers, otherwise they will have to repeat some questions
+function allowContinue(sectionNumber, sectionScore) {
+    let comment = document.querySelector('#section-results' + sectionNumber);
+    comment.style.display = "block";
+    if (sectionScore > 3) {
+        comment.innerHTML = `Well done! You got ${sectionScore}/5`;
+        document.querySelector('#next-button' + sectionNumber).style.display = "inline";
+    } else {
+        comment.innerHTML = `You got ${sectionScore}/5, you should try again to move onto the next section`;
     }
 }
